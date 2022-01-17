208bc89d274151dced07f134ff62399fd44a8b3f
Angus Lee <angus_cc@foxmail.com>
2022-01-17 16:19:50 +0800

    完成了投票应用

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.template.loader import get_template
-from django.http import HttpResponse, Http404
-
-from polls.models import Question
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.db.models import F
+from polls.models import Question, Choice
 
 
 
@@ -21,17 +22,38 @@ def detail(request, question_id):
     # 在Question中寻找匹配question_id的question，未找到则给出404错误
     question = get_object_or_404(Question, pk=question_id)
 
+    # 获取detail.html模板，展示问题内容和选项
     template = get_template('detail.html')
     context = {'question_id': question_id, 'question': question}
-    response = HttpResponse((template.render(context, request)))
+    response = HttpResponse(template.render(context, request))
     return response
 
 def results(request, question_id):
-    response = HttpResponse()
-    response.write("You are looking at the results of question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    template = get_template('results.html')
+    context = {'question': question}
+    response = HttpResponse(template.render(context, request))
     return response
 
+
 def vote(request, question_id):
-    response = HttpResponse()
-    response.write("You are voting on question %s." % question_id)
-    return response
+    question = get_object_or_404(Question, pk=question_id)
+
+    # 可能的异常处理
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 刷新页面，并给出错误信息
+        template = get_template('detail.html')
+        context = {
+            'question': question,
+            'error_message': "You did not select a choice."
+        }
+        response = HttpResponse(template.render(context, request))
+        return response
+    else:
+        # 若正常，用F()函数包裹，避免竞争条件
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # 成功处理POST请求后，都需要进行重定向
+        return HttpResponseRedirect(reverse('polls:results', args = (question_id,)))
