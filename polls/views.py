a783004547923499794a018233b30244571163b3
Angus Lee <angus_cc@foxmail.com>
2022-01-17 12:19:38 +0800

    新增问题展示视图

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.template.loader import get_template
+from django.http import HttpResponse, Http404
 
-from django.http import HttpResponse
+from polls.models import Question
+
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = get_template("index.html")
+
+    # 从Question模型中获取按时间倒序排列的前五项记录，
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    response = HttpResponse(template.render(context, request))
+
+    return response
+
+
+def detail(request, question_id):
+    # 在Question中寻找匹配question_id的question，未找到则给出404错误
+    question = get_object_or_404(Question, pk=question_id)
+
+    template = get_template('detail.html')
+    context = {'question_id': question_id, 'question': question}
+    response = HttpResponse((template.render(context, request)))
+    return response
+
+def results(request, question_id):
+    response = HttpResponse()
+    response.write("You are looking at the results of question %s." % question_id)
+    return response
+
+def vote(request, question_id):
+    response = HttpResponse()
+    response.write("You are voting on question %s." % question_id)
+    return response
